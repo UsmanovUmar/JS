eea6d85531296125ff3c7c9948b77d118e86e246
umar <u@gmail.com>
2022-09-17 22:18:50 +0500

    final

---
+++ script.js
@@ -0,0 +1,27 @@
+var str = prompt ('Введите своё имя:');
+var age = +prompt ('Введите свой возраст:');
+
+console.log(str);
+console.log(age);
+
+var number = +prompt ('432 + 8 = ?');
+var number1 = +prompt ('389 - 9 = ?');
+var number2 = +prompt ('7 * 8 = ?');
+var number3 = +prompt ('100 / 2 = ?');
+var number4 = +prompt ('22 % 5 = ?');
+
+console.log('432 + 8 ='+(432+8)); console.log('Ваш ответ:'+ number);
+console.log('389 - 9 ='+(389-9)); console.log('Ваш ответ:'+ number1);
+console.log('7 * 8 ='+ 7 * 8); console.log('Ваш ответ:'+ number2);
+console.log('100 / 2 ='+ 100 / 2); console.log('Ваш ответ:'+ number3);
+console.log('22 % 5 ='+ 22 % 5); console.log('Ваш ответ:'+ number4);
+
+
+
+var X = prompt('Введите первое число:');
+var Y = prompt('Введите второе число:');
+var Z = prompt('Введите третье число:');
+
+var res = (X + Y + Z) / 3
+
+alert(res)
